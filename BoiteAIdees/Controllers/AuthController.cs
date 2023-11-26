11091836d2b9b95a9303a2d1516375e9f9e966bf
Tommy Dumora <tommydumora@gmail.com>
2023-11-26 03:42:00 +0100

    ajout jwt

---
+++ BoiteAIdees/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using BoiteAIdees.Models.DTOs;
+using BoiteAIdees.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoiteAIdees.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UsersDto>> Login(UsersLogin request)
+        {
+            try
+            {
+                var existingUser = await _authService.GetUserByEmail(request.Email, request.Password);
+
+                if (existingUser == null) return BadRequest($"L'utilisateur avec l'email {request.Email} n'a pas été trouvé.");
+
+                if (!BCrypt.Net.BCrypt.Verify(request.Password, existingUser.PasswordHash)) return BadRequest("Mauvais mot de passe");
+
+                string token = _authService.CreateToken(existingUser);
+
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erreur interne du serveur : " + ex.Message);
+            }
+        }
+    }
+}
---
+++ BoiteAIdees/Services/AuthService.cs
@@ -0,0 +1,77 @@
+﻿using BoiteAIdees.Context;
+using BoiteAIdees.Models.Domaine;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BoiteAIdees.Services
+{
+    public class AuthService
+    {
+        private readonly BoiteAIdeesContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(BoiteAIdeesContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public bool IsPasswordStrong(string password)
+        {
+            if (string.IsNullOrEmpty(password) || password.Length < 6)
+            {
+                return false;
+            }
+
+            bool containsUppercase = password.Any(char.IsUpper);
+            bool containsLowercase = password.Any(char.IsLower);
+            bool containsDigit = password.Any(char.IsDigit);
+
+            return containsUppercase && containsLowercase && containsDigit;
+        }
+
+        public async Task<Users?> GetUserByEmail(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)) throw new ArgumentException("L'adresse e-mail et le mot de passe sont requis.");
+
+            var users = await _context.Users.Where(u => u.Email == email).ToListAsync();
+
+            foreach (var user in users)
+            {
+                if (BCrypt.Net.BCrypt.Verify(password, user.PasswordHash))
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        internal string CreateToken(Users user)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.Name, user.LastName!),
+                //new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
